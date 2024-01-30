38bc7156d84201811f9214e139a97d97dc651515
pawel054 <pawel.marcisz1@gmail.com>
2024-01-30 13:33:29 +0100

    Dodaj ikony elementów listy

--- Lista/Lista/TaskClass.cs
+++ Lista/Lista/TaskClass.cs
@@ -9,6 +9,14 @@ namespace Lista
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Priority { get; set; }
-        public string Image { get; set; }
+        public string Image
+        {
+            get
+            {
+                if (Priority == "Ważne")
+                    return "https://cdn-icons-png.flaticon.com/512/5619/5619095.png";
+                return "https://upload.wikimedia.org/wikipedia/commons/4/43/Minimalist_info_Icon.png";
+            }
+        }
     }
 }
