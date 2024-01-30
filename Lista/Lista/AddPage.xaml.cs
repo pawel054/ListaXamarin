14e29ebc944fbffe3526ec868c449f5d28e18f14
pawel054 <pawel.marcisz1@gmail.com>
2024-01-30 13:26:20 +0100

    Dodaj konstruktory dla edycji i dodawania

--- Lista/Lista/AddPage.xaml.cs
+++ Lista/Lista/AddPage.xaml.cs
@@ -12,14 +12,35 @@ namespace Lista
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddPage : ContentPage
     {
+        List<TaskClass> list;
+        TaskClass model;
         public AddPage(List<TaskClass> list)
         {
             InitializeComponent();
+            this.list = list;
+            addButton.IsVisible = true;
+            editButton.IsVisible = false;
         }
 
         public AddPage(List<TaskClass> list, TaskClass model)
         {
             InitializeComponent();
+            this.list = list;
+            this.model = model;
+            entryTitle.Text = model.Title;
+            chckImportant.IsChecked = model.Priority == "" ? false : true;
+
+            addButton.IsVisible = false;
+            editButton.IsVisible = true;
+        }
+
+        private void AddBtnClicked(object sender, EventArgs e)
+        {
+
+        }
+        private void EditBtnClicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
